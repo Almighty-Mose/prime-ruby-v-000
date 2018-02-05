aa3ca1abcb35046226fd8805c1f394a7a056fa72
Branden Mosley <branden.mosley1337@gmail.com>
2018-02-05 21:58:00 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,11 @@
 require 'benchmark'
-iterations = 100_000
-benchmark.bmdo |bm|
 
-bm.report do
-  
 def prime?(num)
   return false if num <= 1
   (2..Math.sqrt(num)).none? {|i| (num % i).zero?} 
 end
+
+iterations = 100_000
+
+benchmark.bmdo|bm|
+
